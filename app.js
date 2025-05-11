47b4717851d7e6279e088a6052dce8372cd83fa6
Anita-DSM <anahiperezdsm@gmail.com>
2025-05-11 14:56:06 -0300

    variables y función a convertidor

---
+++ app.js
@@ -0,0 +1,6 @@
+const dolar =1300 // (1 dolar va a ser equivalente a 1300 pesos) y va const porque es una variable que tiene que ser fija
+let historial = []; // esto va a guardar cada intento de conversion en mi historial
+let opcion
+function convertirPesosADolares (pesos){
+    return pesos/dolar
+}
